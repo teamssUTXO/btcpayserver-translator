13a31e3fb7e4109264214b3f969815435f009366
Timothé <timothe.fardella@viacesi.fr>
2026-04-16 10:42:16 +0200

    fix : coderabbit suggestion

--- Translator.Tests/QueueHttpMessageHandler.cs
+++ Translator.Tests/QueueHttpMessageHandler.cs
@@ -9,13 +9,14 @@ internal sealed class QueueHttpMessageHandler : HttpMessageHandler
         _responder = responder;
     }
 
-    public int CallCount { get; private set; }
+    private int _callCount;
+    public int CallCount => Volatile.Read(ref _callCount);
 
     public Uri? LastRequestUri { get; private set; }
 
     protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
-        CallCount++;
+        Interlocked.Increment(ref _callCount);
         LastRequestUri = request.RequestUri;
         return Task.FromResult(_responder(request));
     }
